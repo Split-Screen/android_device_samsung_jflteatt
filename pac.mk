a3719f8ebcdfdfeb5044de460659c0ff42b36a75
Lokesh Chamane <lokesh.chamane@pac-rom.com>
2016-06-02 21:51:05 -0400

    jflteatt: PAC-ify
    Change-Id: Ifaac27ba2294afab31e288032b601b3485cb50c2
    Signed-off-by: Lokesh Chamane <lokesh.chamane@pac-rom.com>

--- pac.mk
+++ pac.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/samsung/jflteatt/full_jflteatt.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common PAC stuff.
+$(call inherit-product, vendor/pac/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=jflteuc \
@@ -9,5 +9,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
   BUILD_FINGERPRINT="samsung/jflteuc/jflteatt:5.0.1/LRX22C/I337UCUGOC3:user/release-keys" \
   PRIVATE_BUILD_DESC="jflteuc-user 5.0.1 LRX22C I337UCUGOC3 release-keys"
 
-PRODUCT_NAME := cm_jflteatt
+PRODUCT_NAME := pac_jflteatt
 PRODUCT_DEVICE := jflteatt
